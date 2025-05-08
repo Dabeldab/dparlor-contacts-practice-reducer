d307ed79063b878e34d30e777bdbcbcfb9fdf76c
Dabeldab <1dareios@gmail.com>
2025-05-08 00:09:28 +0000

    added store.js file

--- src/store.js
+++ src/store.js
@@ -1,32 +1,20 @@
 export const initialStore=()=>{
   return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
+  contacts:[]
   }
 }
 
+
 export default function storeReducer(store, action = {}) {
   switch(action.type){
-    case 'add_task':
-
-      const { id,  color } = action.payload
-
-      return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
+      case 'fetchedContacts':
+        const contactArray = action.payload;
+        console.log("in reducer",contactArray)
+        return {
+          ...store,
+          contacts: [...contactArray]
+        }
     default:
-      throw Error('Unknown action.');
+return store
   }    
 }
